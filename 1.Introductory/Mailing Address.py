fb27d2b8057fb47714782e3a98a7ce694b767220
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:56:21 +0530

    20

---
+++ 1.Introductory/Mailing Address.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 18:07:04 2020
+
+@author: OCAC
+"""
+
+print(" Ankit Kumar Mohanty \n ankitmohanty1804@gmail.com \n 8249299474 \n Panasia,Near FC Project \n Jajpur Road \n 755020")
+name="Ankit Kumar Mohanty\n"
+email="ankitmohanty1804@gmail.com\n"
+contact="8249299474\n"
+Address="Panasia,Near FC Project \nJajpur Road \n755020"
+print(name+email+contact+Address)
