02aa77007e7a270aebb3f2db80730f9ff7493928
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:54:52 +0530

    10

---
+++ 1.Introductory/CylinderVolume.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 01:02:05 2020
+
+@author: OCAC
+"""
+from math import pi
+r=float(input("Enter Radius value:"))
+h=float(input("Enter Height value:"))
+volume=pi*(r**2)*h
+
+print("The Volume of the Cylinder is:%.1f"%volume)
