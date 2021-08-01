b86e0485ff86937951b9d0fa7f842c6daafb6c8a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 22:00:03 +0530

    6

---
+++ 2.IF Statements/EvenOdd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 23:56:23 2020
+
+@author: OCAC
+"""
+
+num=int(input("Enter Number:"))
+
+if num%2==0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
