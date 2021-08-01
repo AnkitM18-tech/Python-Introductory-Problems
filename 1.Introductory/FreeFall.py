42c0cc4763c9f4609fced594a93ffdac652d3191
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:55:30 +0530

    15

---
+++ 1.Introductory/FreeFall.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 01:13:30 2020
+
+@author: OCAC
+"""
+
+from math import sqrt
+
+s=float(input("Enter The Height:"))
+a=9.8
+u=0.0
+v=sqrt(u**2+2*a*s)
+
+print("The Speed with The ball is Falling is %.2f"%v)
