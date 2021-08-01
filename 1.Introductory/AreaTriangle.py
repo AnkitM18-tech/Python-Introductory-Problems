4f6a5773f56d1ac14bc655869645ad185fc4ed49
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:54:03 +0530

    3

---
+++ 1.Introductory/AreaTriangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 21:30:20 2020
+
+@author: OCAC
+"""
+
+b=float(input("Enter The Base length:"))
+h=float(input("Enter The height:"))
+area=(b*h)/2
+
+print("The area of the triangle having base",b,"cm and height",h,"cm is %.2f"%area,"cm^2")
