c84e6bd05b95b6d20500abf132772662a22afdcb
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:54:18 +0530

    5

---
+++ 1.Introductory/BMI.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 22:32:45 2020
+
+@author: OCAC
+"""
+
+height=float(input("Enter Height in Meters:"))
+weight=float(input("Enter Weight in KGs:" ))
+BMI=weight/(height*height)
+
+print("BMI:%.4f"%BMI)
