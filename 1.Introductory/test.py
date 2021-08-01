7ca4b7273963f1f0de608d501b40a61cc7b0ef4e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:58:04 +0530

    31

---
+++ 1.Introductory/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 22:12:30 2020
+
+@author: OCAC
+"""
+
+import numpy as np
+arr=np.matrix('1 2;3 4')
+
+print(arr)
+
+a=np.ones((2,3),dtype=int)
+print(a)
