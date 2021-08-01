6079d88a265c4a0215342a92afec927faa36c53e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:55:20 +0530

    14

---
+++ 1.Introductory/FieldArea.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 19:36:01 2020
+
+@author: OCAC
+"""
+
+length=float(input("Enter length:"))
+width=float(input("Enter width:"))
+area=length*width
+area_acres=area/43560
+print("The area of the field is:",area_acres,"acres")
