b8466abb48bf2c3ea6896c9b040143e9ef5867f1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-08-01 21:56:31 +0530

    22

---
+++ 1.Introductory/RoomArea.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 19:26:34 2020
+
+@author: OCAC
+"""
+
+Length=float(input("Enter the Length(in mtrs) :"))
+Width=float(input("Enter the Width(in mtrs) :"))
+Area=Length*Width
+print("The Area Of The Room is :"+str(Area)+"Square Meters")
+print("The Area Of The Room is :",Area,"Square Meters")
